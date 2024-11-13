f026cd1c5d1e0b406d3c8fd1820adbb4a0cc5fa2
willem houm <willem.houm@golem.ai>
2024-11-13 20:14:16 +0100

    switched navar to tsx component

---
+++ front/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type { Tab } from "../interfaces/tab";
+
+const tabs: Tab[] = [
+  { name: "Accueil", href: "/" },
+  { name: "Nos Chats", href: "/nos-chats" },
+];
+console.log(tabs);
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="h-16 bg-orange-300 flex items-center">
+      <img
+        className="h-12 w-12 ml-10"
+        src={`${process.env.REACT_APP_STRAPI_URL}/uploads/Logo_d_811c852867.png`}
+        alt="Logo"
+      />
+      {tabs.map((tab) => (
+        <a
+          key={tab.name}
+          href={tab.href}
+          className="ml-4 text-lg text-gray-800"
+        >
+          {tab.name}
+        </a>
+      ))}
+      <div className="bg-lime-100 flex-grow h-full"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
