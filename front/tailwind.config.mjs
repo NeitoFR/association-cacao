6dd49830a77004c19e5dcbe3991dd964949af294
remousses <remy91490@hotmail.fr>
2024-11-28 23:14:18 +0100

    applied theme

--- front/tailwind.config.mjs
+++ front/tailwind.config.mjs
@@ -3,7 +3,22 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "media",
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+        text: 'var(--text-color)',
+        donate: 'var(--donate-color)',
+        link: 'var(--link-color)',
+        cacaoMeanings: 'var(--cacao-meanings-color)',
+        catCardBorder: 'var(--cat-card-border-color)',
+        catCardBg: 'var(--cat-card-bg-color)',
+        catDiseasesBg: 'var(--cat-diseases-bg-color)',
+        pageTitle: 'var(--page-title-color)',
+        navbar: 'var(--navbar-bg-color)'
+      }
+    },
   },
   plugins: [],
 };
