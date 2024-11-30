c764d2b538d5874b54b3e3d095ac183ef441f9ff
Willem Houm <willem.houm@golem.ai>
2024-11-30 11:18:54 +0100

    Importing font through env vars ok

--- front/tailwind.config.mjs
+++ front/tailwind.config.mjs
@@ -3,21 +3,24 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "media",
   theme: {
-    extend: { 
+    extend: {
+      fontFamily: {
+        glacial: ["Glacial Indifference", "sans-serif"],
+      },
       colors: {
-        primary: 'var(--primary-color)',
-        secondary: 'var(--secondary-color)',
-        tertiary: 'var(--tertiary-color)',
-        text: 'var(--text-color)',
-        donate: 'var(--donate-color)',
-        link: 'var(--link-color)',
-        cacaoMeanings: 'var(--cacao-meanings-color)',
-        catCardBorder: 'var(--cat-card-border-color)',
-        catCardBg: 'var(--cat-card-bg-color)',
-        catDiseasesBg: 'var(--cat-diseases-bg-color)',
-        pageTitle: 'var(--page-title-color)',
-        navbar: 'var(--navbar-bg-color)'
-      }
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        tertiary: "var(--tertiary-color)",
+        text: "var(--text-color)",
+        donate: "var(--donate-color)",
+        link: "var(--link-color)",
+        cacaoMeanings: "var(--cacao-meanings-color)",
+        catCardBorder: "var(--cat-card-border-color)",
+        catCardBg: "var(--cat-card-bg-color)",
+        catDiseasesBg: "var(--cat-diseases-bg-color)",
+        pageTitle: "var(--page-title-color)",
+        navbar: "var(--navbar-bg-color)",
+      },
     },
   },
   plugins: [],
