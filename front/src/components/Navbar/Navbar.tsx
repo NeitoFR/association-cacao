07eebf5840a1d0f253d1bc6dcd755493b47ed1d2
Willem Houm <willem.houm@golem.ai>
2024-12-11 15:31:24 +0100

    front: added donation button. reset details page

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -19,9 +19,8 @@ const Navbar: React.FC<NavbarProps> = ({ tabs }) => {
         className={`flex h-28 flex-col bg-navbar transition-all duration-300 ${isMenuOpen ? "h-64" : ""}`}
       >
         <div className="relative flex min-h-28 w-full items-center">
-          <a href="/">
+          <a className="ml-4 mr-6 h-16 w-16 lg:ml-6 lg:h-24 lg:w-24" href="/">
             <img
-              className="ml-4 mr-6 h-16 w-16 lg:ml-6 lg:h-24 lg:w-24"
               src={`${import.meta.env.PUBLIC_STRAPI_URL}/uploads/logo_d.png`}
               alt="Logo"
             />
@@ -36,6 +35,14 @@ const Navbar: React.FC<NavbarProps> = ({ tabs }) => {
               ))}
             </div>
           </div>
+          <a
+            className="absolute right-0 ml-4 mr-24 size-16 md:mr-6 md:size-20 lg:size-24"
+            href="/"
+          >
+            <div className="dons flex h-full w-full items-center justify-center bg-cover bg-center text-center text-[12px] font-bold text-white md:text-[14px] lg:text-[17px]">
+              FAIRE UN DON
+            </div>
+          </a>
           <button
             className={`hamburger absolute right-0 mr-4 flex h-16 w-16 shrink-0 flex-col items-center justify-center space-y-1 md:hidden ${isMenuOpen ? "active" : ""}`}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
